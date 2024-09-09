d1089b3c35b4e32fd7928aacf6f5219a4bb6f20c
Juan Beltrán <beltranrodriguezjuan9@gmail.com>
2024-09-08 22:12:08 -0500

    Select para las Materias

--- Subject.py
+++ Subject.py
@@ -6,7 +6,14 @@ def readSubjects():
     df = pd.read_csv("data_notas.csv")
     return df
 
-def addSubjects(df, name, mark):
-    df_new = pd.DataFrame([{'Nombre': name, 'Nota': mark}])
+def addSubjects(df, name, note):
+    df_new = pd.DataFrame([{'Nombre': name, 'Nota': note}])
     df = pd.concat([df, df_new], ignore_index=True)
+    saveSubjects(df)
+
+def updateSubjects(df, name, note):
+    df.loc[df['Nombre'] == name, 'Nota'] = note
+    saveSubjects(df)
+
+def saveSubjects(df):
     df.to_csv("data_notas.csv", index=False)
--- app.py
+++ app.py
@@ -15,24 +15,34 @@ df = sub.readSubjects()
 @app.route('/add', methods=['GET', 'POST'])
 def add():
 
+    show = 0;
+
     if request.method == 'POST':
         nameSubject = request.form['name']
         note = request.form['note']
-
         sub.addSubjects(df,nameSubject,note)
 
-    return render_template('updateData.html')
+        return render_template('menu.html')
+
+    return render_template('updateData.html',show=show)
 
 @app.route('/update', methods=['GET', 'POST'])
 def update():
+
+    df = sub.readSubjects()
 
     show = 1
 
     subjects = df['Nombre'].tolist()
 
-    subjectForm = sub.subjectForm(request.form)
+    if request.method == 'POST':
+        nameSubject = request.form.get('name')
+        note = request.form['note']
+        print(note)
+        sub.updateSubjects(df,nameSubject,note)
+        return render_template('menu.html')
 
-    return render_template('updateData.html', show=show, subjects=subjects, subjectForm=subjectForm)
+    return render_template('updateData.html', show=show, subjects=subjects)
 
 if __name__ == '__main__':
     app.run()
